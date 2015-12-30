c9807dc7f688b2924b867a507456520462b30552
calebprior <cprior@tcd.ie>
2015-12-30 12:37:39 +0000

    Basic reading and writing files in bytes

--- src/EchoClient.scala
+++ src/EchoClient.scala
@@ -1,5 +1,6 @@
 import java.io.PrintWriter
 import java.net.{InetAddress, Socket}
+import java.nio.file.{Paths, Files}
 
 import scala.io.BufferedSource
 
@@ -8,17 +9,25 @@ import scala.io.BufferedSource
  */
 object EchoClient {
   def main(args: Array[String]) {
-    val s = new Socket(InetAddress.getByName("178.62.121.108"), 443)
-    lazy val in = new BufferedSource(s.getInputStream).getLines()
-    val out = new PrintWriter(s.getOutputStream)
+//    val s = new Socket(InetAddress.getByName("178.62.121.108"), 443)
+//    lazy val in = new BufferedSource(s.getInputStream).getLines()
+//    val out = new PrintWriter(s.getOutputStream)
+//
+//    val message = io.StdIn.readLine("Enter Message> ")
+//    out.println(message + "\n")
+//    out.flush()
+//
+//    println("Sent: " + message)
+//    while(in.hasNext)
+//      println("Received: " + in.next)
+//    s.close()
 
-    val message = io.StdIn.readLine("Enter Message> ")
-    out.println(message + "\n")
-    out.flush()
+    val message = io.StdIn.readLine("Enter FileName> ")
 
-    println("Sent: " + message)
-    while(in.hasNext)
-      println("Received: " + in.next)
-    s.close()
+    var byteArray = Files.readAllBytes(Paths.get(message))
+
+    println(byteArray)
+
+    Files.write(Paths.get("woo.txt"), byteArray)
   }
 }
