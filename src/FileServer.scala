2df0ee0656e0c58f811bfda970a10ee530ebf6cf
calebprior <cprior@tcd.ie>
2016-01-23 20:27:00 +0000

    Adding fileserver classes

---
+++ src/FileServer.scala
@@ -0,0 +1,26 @@
+import java.net.{ServerSocket, Socket}
+import java.util.concurrent.Executors
+
+/**
+  * Created by Caleb Prior on 23-Jan-16.
+  */
+class FileServer extends ServerTrait{
+  def setup(args: Array[String]): Unit = {
+    val portNumber = args(0)
+
+    try{
+      port = Integer.parseInt(portNumber)
+      serverSocket = new ServerSocket(port)
+      pool = Executors.newFixedThreadPool(poolSize)
+    } catch {
+      case e : Exception =>
+        println("SERVER: ERROR - " + e.getMessage)
+        System.exit(0)
+    }
+  }
+
+  def acceptNewConnection(socket: Socket): Unit = {
+    val socketHandler = new SocketHandler(socket)
+    pool.execute(new ServerListener(socketHandler, this, new FileServerMessageHandler(socketHandler)))
+  }
+}
---
+++ src/FileServerMessageHandler.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by Caleb Prior on 23-Jan-16.
+  */
+class FileServerMessageHandler(socketHandler: SocketHandler) extends MessageHandler(socketHandler) {
+  def handleMessage(msg: String): Unit = {
+
+  }
+}
